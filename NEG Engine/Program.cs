634ca56ca22161513320d263c862dad1f1807026
Costinel Ghita <irinel132@gmail.com>
2019-03-07 12:22:34 +0000

    1
    first commit

---
+++ NEG Engine/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+using System.Drawing;
+
+namespace NEG_Engine
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form gameWindow = new Form();
+
+            gameWindow.BackColor        = Color.White;
+            gameWindow.FormBorderStyle  = FormBorderStyle.None;
+            gameWindow.Bounds           = Screen.PrimaryScreen.Bounds;
+            gameWindow.TopMost          = true;
+
+            Application.EnableVisualStyles();
+            Application.Run(gameWindow);
+
+
+        }
+    }
+}
