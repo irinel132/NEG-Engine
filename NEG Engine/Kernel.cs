10dc7e27b48625cba549bf21ebf6c39026789d3c
Costinel Ghita <irinel132@gmail.com>
2019-03-07 12:41:04 +0000

    Work start
    Basic methods added

--- NEG Engine/Kernel.cs
+++ NEG Engine/Kernel.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace NEG_Engine
 {
     class Kernel
     {
+        // internal variables
+        protected Form _gameWindow = null;
 
+
+
+        public Kernel (Form F)
+        {
+            this._gameWindow = F;
+        }
     }
 }
