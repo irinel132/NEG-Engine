9c1861504e5130df7eaad1359783a4f583e6bd81
orrewingh <orrewingh@gmail.com>
2019-03-07 15:08:38 +0000

    Interfaces
    Setup the interfaces, to the classes all empty but foundations are there

---
+++ NEG Engine/Hitbox/IHitbox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace NEG_Engine.Managers
+{
+    interface IHitbox
+    {
+       Point IGetLocation();
+
+        Point ISetLocation();
+
+        void IGetShape();
+
+        void ISetShape();
+
+        Boolean ICollidesWith();
+    }
+}
