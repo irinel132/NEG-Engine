8565d45deae66045131da593237e22cbd99218ce
Costinel Ghita <irinel132@gmail.com>
2019-03-07 14:28:02 +0000

    Pre Chaos

---
+++ NEG Engine/Render/BasicRender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace NEG_Engine.Render
+{
+    class BasicRender : IRender
+    {
+        // Internal Variables
+        protected Graphics  _graphics   = null;
+        protected Form      _form       = null;
+        // Constructor
+
+        public BasicRender (Form Form)
+        {
+            _form       = Form;                     // Assign the form to a variable
+            _graphics   = Form.CreateGraphics();    // Get the graphics from the form and assign it to a variable
+        }
+
+        // Methods
+        public void DrawBitmap(Bitmap BMP, Point Position)
+        {
+            _graphics.DrawImage(BMP, Position);
+        }
+    }
+}
---
+++ NEG Engine/Render/IRender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+namespace NEG_Engine.Render
+{
+    interface IRender
+    {
+        void DrawBitmap (Bitmap BMP, Point Position);
+    }
+}
