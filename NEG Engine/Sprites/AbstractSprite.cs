4deb61c9bde3b47966d6c2226d9e58c92e923144
Costinel Ghita <irinel132@gmail.com>
2019-03-08 15:21:30 +0000

    ISprite and AbstractSprite classes created

--- NEG Engine/Sprites/AbstractSprite.cs
+++ NEG Engine/Sprites/AbstractSprite.cs
@@ -11,21 +11,51 @@ namespace NEG_Engine.Sprites
         // Internval variables
         protected   Bitmap      _sprite         = null;                 // The actual Bitmap sprite
 
-        protected   Point       _position;                              // 
-        protected   Point       _scale;                                 // 
+        protected   Point       _position;                              // The in engine position of the Sprite
+        protected   Point       _scale;                                 // The scale of the sprite in X and Y axises. 100 is normal
 
-        protected   bool _      flipped         = null;                 // 
-        protected   float       _rotation       = null;                 // 
+        protected   bool        _flipped        = false;                // True if the sprite is flipped vertically
+        protected   float       _rotation       = 0;                    // The rotation of the sprite in degrees
 
-        // Constructors
+        // Constructors     
+        public AbstractSprite(Bitmap Sprite, Point Position, Point Scale, bool Flipped, float Rotation)
+        {
+            // Asign the arguments to the internal variables
+            _sprite         = Sprite;
 
+            _position       = Position;
+            _scale          = Scale;
+
+            _flipped        = Flipped;
+            _rotation       = Rotation;
+        }        
 
         // Interface Methods
-        public Bitmap Sprite { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Point Scale { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Point Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public float Rotation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Flipped { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Bitmap Sprite
+        {
+            get { return _sprite;       }
+            set { _sprite = value;      }
+        }
+        public Point Scale
+        {
+            get { return _scale;        }
+            set { _scale = value;       }
+        }
+        public Point Position
+        {
+            get { return _position;     }
+            set { _position = value;    }
+        }
+        public float Rotation
+        {
+            get { return _rotation;     }
+            set { _rotation = value;    }
+        }
+        public bool Flipped
+        {
+            get { return _flipped;      }
+            set { _flipped = value;     }
+        }
 
 
         // Methods
