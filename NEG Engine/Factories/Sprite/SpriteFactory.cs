5486488d3bea7675f504ae766d1f7f1b0c56df56
Costinel Ghita <irinel132@gmail.com>
2019-03-21 12:30:18 +0000

    1
    1

---
+++ NEG Engine/Factories/Sprite/SpriteFactory.cs
@@ -0,0 +1,13 @@
+﻿using NEG_Engine.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NEG_Engine.Factories.Sprite
+{
+    class SpriteFactory : AbstractFactory<ISprite>
+    {
+        
+    }
+}
